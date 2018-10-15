7338b5dbd7db24f732fa2b503da840bfb4376107
Philippe Borg <philippe.borg.pro@gmail.com>
2018-10-15 02:23:03 +0200

    issue with fetching data, action fetchFailure works (at least)

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Axios from 'axios'
 
-import Config from '@config/config'
+import Config from '@config/Config'
 import { MentionList } from '@components/containers'
-import { Mention } from '@actions/Mention'
+import { Mention } from '@actions/'
 
 class App extends Component {
 
   componentDidMount () {
     // FETCH MENTION
-    const bearer = 'Bearer ' + Config.mentionAPI.token;
+    const bearer = 'Bearer ' + Config.mentionAPI.token
+    const url = Config.mentionAPI.endpoint(Config.mentionAPI.accountId, Config.mentionAPI.alertId, Config.mentionAPI.token)
 
+    console.log(bearer)
+    console.log(url)
     const obj = {
-      method: 'GET',
-      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': bearer
       }
     };
 
-    fetch(Config.mentionAPI.endpoint(Config.mentionAPI.accountId, Config.mentionAPI.alertId, Config.mentionAPI.token), obj)
-    .then(response => console.log(response));
+    fetch(url, obj)
+    .then(response => console.log(response))
+    .catch(error => this.props.fetchFailure())
   }
 
   render() {
