8978b66672148a059d930100ff8ac64cc0ff7f8a
Philippe Borg <philippe.borg.pro@gmail.com>
2018-10-20 06:28:52 +0200

    highlight word ongoing

--- src/components/organisms/MentionCard/MentionCard.js
+++ src/components/organisms/MentionCard/MentionCard.js
@@ -2,7 +2,6 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 import { memoize } from 'lodash'
-import { Highlightable } from 'highlightable'
 
 import { MentionIcon } from '@components/molecules'
 import { Text, Title } from '@components/atoms'
@@ -11,6 +10,7 @@ import { MentionCardContainer, ContentContainer, TextContainer, MentionUrl, Head
 
 const formatDate = (date) => moment(date).format('D MMM')
 const memoizedFormatDate = memoize(formatDate)
+const isMultiple4 = (n) => n % 4
 
 class MentionCard extends PureComponent {
 
@@ -20,6 +20,28 @@ class MentionCard extends PureComponent {
     const CheckDate = isRead ? <MentionDate isRead>{dateFormated}</MentionDate> : <MentionDate>{dateFormated}</MentionDate>
     const CheckContent = isRead ? <MentionContent isRead>{content}</MentionContent> : <MentionContent>{content}</MentionContent>
     const CheckUrl = isRead ? <MentionUrl isRead>{url}</MentionUrl> : <MentionUrl>{url}</MentionUrl>
+
+    // description_short
+    //title
+    let offsetTitle = []
+
+    if (isMultiple4(offsets.title.length) === 0 && offsets.title.length > 1) {
+      //Things to highligh
+      for (let i = 0; i < offsets.title.length; i += 4) {
+        let objectRange = {
+          start: offsets.title[i],
+          end: offsets.title[i] + offsets.title[i + 2]
+        }
+
+        offsetTitle.push(objectRange)
+      }
+    }
+
+    console.log('offsetTitle: ', offsetTitle);
+    console.log(title)
+
+
+
 
     return (
       <MentionCardContainer>
--- src/redux/mentions/thunks.js
+++ src/redux/mentions/thunks.js
@@ -6,7 +6,6 @@ export const FetchMentionsThunk = () => async (dispatch, getState) => {
   dispatch(fetchMentions.pending())
   try {
     const { data } = await api.fetchMentions()
-    console.log(data.mentions[0]);
     dispatch(fetchMentions.success({ mentions: data.mentions }))
     return data.mentions
   } catch (e) {
